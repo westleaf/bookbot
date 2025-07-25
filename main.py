90bfbab65b20ca5be2b6da4a576a986a3d5bf6cc
ver-joakim <joakim.westblad@vermiculus.se>
2025-07-25 19:57:07 +0200

    some work

--- main.py
+++ main.py
@@ -7,7 +7,11 @@ def get_book_text(path_to_file):
 def main():
   num_words = len(get_num_words(get_book_text("books/frankenstein.txt")))
   num_char = get_num_char(get_book_text("books/frankenstein.txt"))
+  sorted_list = get_sorted_list(num_char)
   print(f"{num_words} words found in the document")
   print(f"{num_char}")
+  print(f"{sorted_list}")
+  for i in range(0, len(sorted_list)):
+    print(sorted_list[i])
 
 main()
--- stats.py
+++ stats.py
@@ -8,5 +8,15 @@ def get_num_char(text_to_read):
       word_dict[char.lower()] = 1
     else:
       word_dict[char.lower()] += 1
-
   return word_dict
+
+def sort_on(items):
+  return items["num"]
+
+def get_sorted_list(dict_to_sort):
+    sorted_dict_list=[]
+    for key, value in dict_to_sort.items():
+      sorted_dict_list.append({"char":key, "num":int(value)})
+
+    sorted_dict_list.sort(key=sort_on)
+    return sorted_dict_list
